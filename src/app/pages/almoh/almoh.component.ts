6e2263155abb4fd491ef86b662028b69f3cad076
LucaTB <lucatourn@gmail.com>
2022-08-27 12:30:43 -0300

    first commit web deployment

---
+++ src/app/pages/almoh/almoh.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-almoh',
+  templateUrl: './almoh.component.html',
+  styleUrls: ['./almoh.component.scss']
+})
+export class AlmohComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
